f07c29e85f9da686481539c5b8f4b1cb38918b72
Po-Chun Wu <pcwu2022@gmail.com>
2025-03-30 18:34:07 +0800

    edited admin

--- lifeos_system/admin.py
+++ lifeos_system/admin.py
@@ -7,6 +7,15 @@ from notion_manager import NotionManager
 from synced_document import SyncedDocumentManager
 from cache import Cache
 from request_manager import RequestManager
+import json
+
+async def fetch_crms(db_manager):
+    f = db_manager.db["crms"].find({})
+    arr = []
+    for x in f:
+        arr.append(str(x))
+    with open("crms.json", "w") as file:
+        file.write(json.dumps(arr, indent=4, ensure_ascii=False))
 
 if __name__ == "__main__":
     db_manager = DBManager()
@@ -14,3 +23,5 @@ if __name__ == "__main__":
         command = input("> ")
         if command == "db_manager.delete_everything()":
             asyncio.run(db_manager.delete_everything())
+        if command == "fetch_crms()":
+            asyncio.run(fetch_crms(db_manager))
